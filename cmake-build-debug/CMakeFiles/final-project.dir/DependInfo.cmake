6221f5626a438b959b1e5f0aa9912b1fe2af971c
Serena Gong <sxgong2@illinois.edu>
2022-05-06 22:14:02 -0500

    CMake Files

---
+++ cmake-build-debug/CMakeFiles/final-project.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/serenagong/Downloads/Cinder/my-projects/final-project-syxrg/apps/cinder_app_main.cc" "/Users/serenagong/Downloads/Cinder/my-projects/final-project-syxrg/cmake-build-debug/CMakeFiles/final-project.dir/apps/cinder_app_main.cc.o"
+  "/Users/serenagong/Downloads/Cinder/my-projects/final-project-syxrg/src/Board.cpp" "/Users/serenagong/Downloads/Cinder/my-projects/final-project-syxrg/cmake-build-debug/CMakeFiles/final-project.dir/src/Board.cpp.o"
+  "/Users/serenagong/Downloads/Cinder/my-projects/final-project-syxrg/src/connect_four_app.cpp" "/Users/serenagong/Downloads/Cinder/my-projects/final-project-syxrg/cmake-build-debug/CMakeFiles/final-project.dir/src/connect_four_app.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FT2_BUILD_LIBRARY"
+  "FT_DEBUG_LEVEL_TRACE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/Users/serenagong/Downloads/Cinder/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
